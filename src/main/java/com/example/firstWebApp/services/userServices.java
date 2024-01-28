0d6944500a933272e3a3ed644e5d146498e891d9
L.B.Asoftware <enge.sibrahim@gmail.com>
2024-01-28 08:52:17 +0200

    update html file

--- src/main/java/com/example/firstWebApp/services/userServices.java
+++ src/main/java/com/example/firstWebApp/services/userServices.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 @Service
 public class userServices {
@@ -19,5 +20,9 @@ public class userServices {
     public ArrayList<user> getAll()
     {
         return (ArrayList<user>) repository.findAll();
+    }
+    public Optional<user> findUserById(Long id)
+    {
+        return repository.findById(id);
     }
 }
